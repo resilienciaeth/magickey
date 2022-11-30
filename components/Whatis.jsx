8ba24ef3ac7ef87e437278ae44806d7e6dbeb3d5
resilienciaeth <106503220+resilienciaeth@users.noreply.github.com>
2022-11-30 17:01:53 -0500

    adjustments-to-cards

--- components/Whatis.jsx
+++ components/Whatis.jsx
@@ -7,8 +7,8 @@ function Whatis() {
         <p className="text-center text-[10px] md:text-[15px] magickey-1">From books to ebooks to debooks.</p>
         <h1 className="magickey-1 text-center text-[36px] md:text-[70px] font-bold">What is DEBOOK?</h1>
         <div className="h-[90vh] nm:px-20 space-y-4 nm:space-y-0 nm:space-x-14 flex flex-col nm:flex-row mt-10">
-          <div className=" orange-1 h-[33%] nm:h-[40vh] nm:w-[33%]  rounded-xl flex items-center justify-start flex-col">
-            <h1 className="mt-10 nm:mt-16 text-[20px] md:text-[24px] text-white text-center font-bold leading-[30px]">
+          <div className=" orange-1 h-[33%] nm:h-[40vh] nm:w-[33%]  rounded-xl flex items-center justify-start nm:justify-center flex-col">
+            <h1 className="mt-10 nm:mt-0 text-[20px] md:text-[24px] text-white text-center font-bold leading-[30px]">
               A book is a book
               <br />
               {' '}
@@ -18,8 +18,8 @@ function Whatis() {
               A platform that will give you access to read all the authors you love with a one-payment service. Like the Spotify of books.
             </p>
           </div>
-          <div className="  orange-1 h-[33%] nm:h-[40vh] nm:w-[33%] rounded-xl flex items-center justify-start flex-col">
-            <h1 className="mt-10 nm:mt-16 text-[20px] md:text-[24px] text-white text-center font-bold leading-[30px]">
+          <div className="  orange-1 h-[33%] nm:h-[40vh] nm:w-[33%] rounded-xl flex items-center justify-start nm:justify-center flex-col">
+            <h1 className="mt-10 nm:mt-0 text-[20px] md:text-[24px] text-white text-center font-bold leading-[30px]">
               A book is a book
               <br />
               {' '}
@@ -29,8 +29,8 @@ function Whatis() {
               Each book has a social layer to connect you with other passionate readers. Being an author will not be just about writing but keeping the book alive.
             </p>
           </div>
-          <div className="orange-1 h-[33%] nm:h-[40vh] nm:w-[33%] rounded-xl flex items-center justify-start flex-col">
-            <h1 className="mt-10 nm:mt-16 text-[20px] md:text-[24px] text-white text-center font-bold leading-[30px]">
+          <div className="orange-1 h-[33%] nm:h-[40vh] nm:w-[33%] rounded-xl flex items-center justify-start nm:justify-center flex-col">
+            <h1 className="mt-10 nm:mt-0 text-[20px] md:text-[24px] text-white text-center font-bold leading-[30px]">
               A book is a book
 
               <br />
