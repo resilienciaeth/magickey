f871f3039d0b8e84bb0c6e01e486b2db532f47bb
resilienciaeth <106503220+resilienciaeth@users.noreply.github.com>
2022-12-27 18:20:59 -0500

    magickey-image

--- components/Comparation.jsx
+++ components/Comparation.jsx
@@ -1,10 +1,11 @@
+import { CldImage } from 'next-cloudinary';
 import React from 'react';
 
 function Comparation() {
   return (
-    <div className="h-screen">
-      <div className="nm:w-[60%] px-10">
-        <h1 className="text-[30px] font-bold nm:text-[70px] magickey-7">
+    <div className="flex flex-col nm:flex-row nm:h-[90vh]">
+      <div className="nm:w-[50%] flex flex-col justify-center px-10">
+        <h1 className="text-[30px] font-bold nm:text-[50px] magickey-7">
           The differences between
           {' '}
           <br />
@@ -12,7 +13,7 @@ function Comparation() {
           DEBOOK and 'debook'
         </h1>
         <div className=" mt-10">
-          <p className="text-[#9F8A81] text-[15px] nm:text-[26px]">
+          <p className="text-[#9F8A81] text-[15px] ">
             <span className="font-bold text-[#9F8A81]  ">DEBOOK</span>
             {' '}
             is the platform, Marketplace and tokenization tool.
@@ -21,12 +22,15 @@ function Comparation() {
             {' '}
             is the term we use to refer to the brand. The future of books.
           </p>
-          <p className="mt-10 text-[#9F8A81] text-[15px] nm:text-[26px]">
+          <p className="mt-10 text-[#9F8A81] text-[15px] ">
             <span className="font-bold">“debook”</span>
             {' '}
             is the new book’s format, a book that you can use to read, watch, interact with, own and exchange. A digital collectible that grants you access to a private community around that book. A new version of books.
           </p>
         </div>
+      </div>
+      <div className="nm:w-[50%] flex items-start">
+        <CldImage src="v1672182763/Libro_rr2okq.png" width={2250} height={3394} />
       </div>
     </div>
   );
