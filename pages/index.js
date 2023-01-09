5225c72ae7331145dfd0cfb77e705f09ac7c5def
resilienciaeth <106503220+resilienciaeth@users.noreply.github.com>
2023-01-09 10:38:17 -0500

    <meta> tags

--- pages/index.js
+++ pages/index.js
@@ -17,12 +17,22 @@ import Footer from '../components/Footer';
 const index = () => (
   <div>
     <Head>
-      <title>DEBOOK MAGICKEY - Home</title>
+
+      <title>DEBOOK MAGICKEY</title>
+      <meta name="title" content="DEBOOK MAGICKEY" />
       <meta name="description" content="The future of books. Get a MAGICKEY now and join us on creating the future of books" />
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site_id" content="1547000128434835464" />
-      <meta name="twitter:title" content="The future of books" />
-      <meta name="twitter:image" content="https://res.cloudinary.com/drxuutjwr/image/upload/v1672258550/debook-art_yawpmi.png" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://debookmagickey.io/" />
+      <meta property="og:title" content="DEBOOK MAGICKEY" />
+      <meta property="og:description" content="The future of books. Get a MAGICKEY now and join us on creating the future of books" />
+      <meta property="og:image" content="https://res.cloudinary.com/drxuutjwr/image/upload/v1673278645/Screen_Shot_2023-01-08_at_3.08.20_PM_ckvihg.png" />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://debookmagickey.io/" />
+      <meta property="twitter:title" content="DEBOOK MAGICKEY" />
+      <meta property="twitter:description" content="The future of books. Get a MAGICKEY now and join us on creating the future of books" />
+      <meta property="twitter:image" content="https://res.cloudinary.com/drxuutjwr/image/upload/v1673278645/Screen_Shot_2023-01-08_at_3.08.20_PM_ckvihg.png" />
 
     </Head>
     <Navbar />
